9899d41748bef048588d4e50bc01346bba7d8964
soy eddy <soyeddy.email.com>
2024-12-27 18:04:44 -0600

    pruebas GifGrid

--- src/constants/constantes.js
+++ src/constants/constantes.js
@@ -1,5 +1,5 @@
-export const API_URL = import.meta.env.VITE_API_URL;
-export const API_KEY = import.meta.env.VITE_API_KEY;
+export const API_URL = process.env.VITE_API_URL;
+export const API_KEY = process.env.VITE_API_KEY;
 
 
 
--- src/tests/components/GifGrid.test.js
+++ src/tests/components/GifGrid.test.js
@@ -1,3 +1,11 @@
-describe('Pruebas en el componente <GifGrid/>', () => { 
-    test('should first', () => { second })
- })
+import { render, screen } from "@testing-library/react";
+import { GifGrid } from "../../components/GifGrid";
+
+describe("Pruebas en el componente <GifGrid/>", () => {
+  const category = "One Punch";
+  test("Debe de moestrar el loading inicialmente", () => {
+    render(<GifGrid category={category} />);
+    expect(screen.getByText("Cargando..")); //esperamos que el texto cargando exista
+    screen.debug();
+  });
+});
